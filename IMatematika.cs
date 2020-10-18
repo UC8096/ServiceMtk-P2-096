8fc04e77b5a71410ba90ec3f3435e9a651706a0e
UC8096 <vyaninsya.nurmuhammad@gmail.com>
2020-10-18 16:30:19 +0700

    Add project files.

---
+++ IMatematika.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using static ServiceMtk_P2_096.Matematika;
+
+namespace ServiceMtk_P2_096
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IMatematika
+    {
+        [OperationContract]
+        /* output->*/
+        int Tambah(int a, int b); //method
+        [OperationContract]
+        int Kurang(int a, int b); //input
+        [OperationContract]
+        int Kali(int a, int b);
+        [OperationContract]
+        int Bagi(int a, int b);
+        [OperationContract]
+        Koordinat TKoordinat(Koordinat a, Koordinat b); //object dari class 
+
+    }
+}
